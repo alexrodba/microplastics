a8a048d6617701926fbb133e3fa3cc591d27594a
Àlex Rodríguez Bacardit <alex.rodriguez@marsbased.com>
2025-06-05 16:41:14 +0200

    fixing check isProd

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
 
 const nextConfig = {
   output: 'export', // Enables static site generation
   images: {
     unoptimized: true, // Required for static export
   },
-  basePath: isProd ? '/microplastics' : '', // Only use basePath in production - Required for Github Pages
+  basePath: isGithubActions ? '/microplastics' : '', // Use basePath on GitHub Pages
 };
 
 export default nextConfig;
