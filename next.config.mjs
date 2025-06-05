7716bf40927136ee749779d0ef00c3956b5fafca
Àlex Rodríguez Bacardit <alex.rodriguez@marsbased.com>
2025-06-05 17:17:00 +0200

    trying something else

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
-const basePath = process.env.BASE_PATH || '';
-console.log('Using basePath:', basePath);
-
 const nextConfig = {
   output: 'export', // Enables static site generation
   images: {
     unoptimized: true, // Required for static export
   },
-  basePath
+  basePath: process.env.BASE_PATH || '',
 };
 
 export default nextConfig;
